b7f7cd770920399cda3a87b9e918398e76a0937d
soonregii <soonregii@gmail.com>
2024-12-19 20:16:04 +0900

    Create api.js

---
+++ netlify/functions/api.js
@@ -0,0 +1,10 @@
+exports.handler = async (event, context) => {
+  const { name } = event.queryStringParameters || {};
+
+  return {
+    statusCode: 200,
+    body: JSON.stringify({
+      message: `Hello, ${name || 'Guest'}!`,
+    }),
+  };
+};
